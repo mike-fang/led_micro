1ce43357497f5d320a8668edccce10e1fc86221c
michael <1michaelfang@gmail.com>
2020-06-29 17:39:25 -0700

    rasp-pi works

--- scratch_pad.py
+++ scratch_pad.py
@@ -1,17 +1,15 @@
 import cv2
 
 cap = cv2.VideoCapture(0)
-cap.set(3, 640*2)
-cap.set(4, 480*2)
-for n in range(100):
-    print(n, cap.get(n))
-cap.set(8, -20)
+print(cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 1))
+print(cap.set(cv2.CAP_PROP_EXPOSURE, 2000))
+print(cap.get(cv2.CAP_PROP_EXPOSURE))
 while True:
-    ret, frame = cap.read()
-    print(frame)
-    cv2.imshow('', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+	ret, frame = cap.read()
+	cv2.imshow('', frame)
+	print(cap.get(cv2.CAP_PROP_EXPOSURE))
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
 cam.release()
 cv2.destroyAllWindows()
 assert False
